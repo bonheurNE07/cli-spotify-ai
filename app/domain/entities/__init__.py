b738fab5e8936613678598c8917deead01a54ae3
bonheurNE07 <bonheurndezenc@gmail.com>
2026-02-28 18:27:10 +0200

    exposed all entities on__init__

---
+++ app/domain/entities/__init__.py
@@ -0,0 +1,6 @@
+from app.domain.entities.track import Track
+from app.domain.entities.user import User
+from app.domain.entities.playlist import Playlist
+from app.domain.entities.recommendation import RecommendationResult
+
+__all__ = ["Track", "User", "Playlist", "RecommendationResult"]

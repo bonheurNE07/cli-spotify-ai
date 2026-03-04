1ab6a1f1b0c37def82ea1a58405c57e5cef9500f
bonheurNE07 <bonheurndezenc@gmail.com>
2026-03-04 15:46:18 +0200

    implemented lyrics repository abstraction, this will help for external lyrics providers

---
+++ app/domain/repositories/lyrics_repository.py
@@ -0,0 +1,7 @@
+from abc import ABC, abstractmethod
+
+class LyricsRepository(ABC):
+    @abstractmethod
+    def get_lyrics(self, artist: str, title: str) -> str | None:
+        """Fetch lyrics for a given artist and track title."""
+        raise NotImplementedError

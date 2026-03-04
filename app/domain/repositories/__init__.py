c21b4ea26c04577d64dd8785e34ee979c59d7c3c
bonheurNE07 <bonheurndezenc@gmail.com>
2026-03-04 15:55:34 +0200

    formating done

--- app/domain/repositories/__init__.py
+++ app/domain/repositories/__init__.py
@@ -1,5 +1,5 @@
-from app.domain.repositories.spotify_repository import SpotifyRepository
 from app.domain.repositories.lyrics_repository import LyricsRepository
+from app.domain.repositories.spotify_repository import SpotifyRepository
 from app.domain.repositories.user_repository import UserRepository
 
 __all__ = ["SpotifyRepository", "LyricsRepository", "UserRepository"]
--- app/domain/repositories/lyrics_repository.py
+++ app/domain/repositories/lyrics_repository.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 
+
 class LyricsRepository(ABC):
     @abstractmethod
     def get_lyrics(self, artist: str, title: str) -> str | None:
